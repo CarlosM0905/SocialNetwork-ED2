eeec84277c255070b4bb53e341621b4546ec8f7a
CarlosM0905 <carlos1843216@gmail.com>
2019-03-10 23:38:30 -0500

    Routing and Components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: LoginComponent},
+  {path:"login", component: LoginComponent},
+  {path:"register", component: RegisterComponent},
+  {path:"**", component: LoginComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
